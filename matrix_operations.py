b7f1c34678c9275caf400d8e64c6adcba019586f
ipragmatic <ipragmatic2r@gmail.com>
2023-04-09 17:12:16 -0400

    Added matrix subtraction, modified TODO list, changed display_matrix function

--- matrix_operations.py
+++ matrix_operations.py
@@ -7,12 +7,13 @@ Defines a Matrix class that allows mathematical operations (addition, subtractio
     ]
 
 """
-#1 TODO: Create subtraction function(s)
-#2 TODO: Create multiplication function(s)
-#1 TODO: Don't allow the creation of matrices that have rows and/or columns with different lengths.
-#2 TODO: Handle errors that occur from improper matrix addition.
-#3 TODO: Handle errors raised when user passes improper parameters into class constructor
-#4 TODO: Figure out how to better implement functions into __init__ function
+#TODO: Create multiplication function (look @ function definiton to see what needs to be built).
+#TODO: Add self.determinant attribute to Matrix class.
+#TODO: Don't allow the creation of matrices that have rows and/or columns with different lengths.
+#TODO: Handle errors that occur from improper matrix addition.
+#TODO: Handle errors raised when user passes improper parameters into class constructor.
+#TODO: Figure out how to create a function that determines how the matrix is created when class 
+#      constructor is called.
 
 
 class Matrix:
@@ -44,10 +45,17 @@ class Matrix:
 
         self.rows = len(self.matrix)
         self.cols = len(self.matrix[0])
+        #self.determinant = None
+
+
+    def create_mtrx(self):
+        """ I really want to create a function that determines how to create matrix when class
+            constructor is called. It will make the __init__ function look cleaner. """
+        pass
 
 
     def mtrx_add(self, *args):
-        """ Performs addition on any number of matrices put into parameter field, and returns
+        """ Performs addition on any number of matrices put into parameter field and returns
             the sum of all the matrices. """
             
         for mtrx in args:
@@ -61,11 +69,44 @@ class Matrix:
                     self.matrix[row][i] += mtrx[row][i]
         return self.matrix
 
+    
+    def mtrx_subt(self, *args):
+        """ Performs subtraction on any number of matrices put into parameter field and returns 
+            the difference of all the matrices. """
+
+        for mtrx in args:
+            if type(mtrx) == type(list()):
+                pass
+            else:
+                mtrx = mtrx.matrix
+            for row,entries in enumerate(mtrx):
+                for i in range(len(entries)):
+                    self.matrix[row][i] -= mtrx[row][i]
+        return self.matrix
+
+
+    def mtrx_mult(self, *args):
+        """ Performs multiplication on any number of matrices and/or scalar multiples in parameter
+            field and returns the product of all the matrices.
+
+            The returned product will be assigned to the self.matrix attribute, which means that 
+            there must be a Matrix object that this function is being called on (obviously).
+
+            *** Further documentation will be provided once the definition is created. *** """
+        # TODO: Build a type check for each individual element being looped through in *args. Check
+        #       if element is an array type or a float (matrices can be multiplied by a float).
+        # TODO: For matrix multiplication, figure out how to multiply each entry in the row of 
+        #       one matrix by each entry in the column of another matrix, find the sum of each
+        #       product, and place the sum in the corresponding entry of product matrix.
+        # TODO: Maybe to some kind of list comprehension for scalar multiples, shouldn't be too complex.
+
+        pass
+
 
     def display_matrix(self):
         """ Prints the specified matrix in a somewhat prettier way. """
         pretty = f"~~~~~ {self.rows}x{self.cols} Matrix ~~~~~"
-        print("\n"*2, pretty)
+        print(pretty)
         print(*self.matrix, sep="\n")
         print("-" * len(pretty))
         
